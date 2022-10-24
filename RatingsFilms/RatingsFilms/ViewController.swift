9746107a2174a5a68a4932bc9fc696518fdd2b1d
Валентин Коскин <>
2022-10-24 22:17:05 +0400

    init project(SwiftLint)

---
+++ RatingsFilms/RatingsFilms/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RatingsFilms
+//
+//  Created by Валентин Коскин on 24.10.2022.
+//
+
+import UIKit
+
+/// main vc
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
